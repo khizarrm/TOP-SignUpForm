76dae7af3119492c401e99564c1c8b443e8c06a2
Khizar Malik <khizarmalik2003@gmail.com>
2024-06-05 08:28:46 -0400

    Finished up front-end, and improved back end. User's now need to have the same password in order to submit.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const submitButton = document.querySelector('button');
 submitButton.addEventListener('click', (event) => {
-    const password = document.querySelector('input[type=pword');
-    const pswordConfirm = document.querySelector('input[type=pcword]');
+    const password = document.querySelector('input[type=password][name=password');
+    const pswordConfirm = document.querySelector('input[type=password][name=password-confirm]');
 
-    if (password != pswordConfirm){
+    if (password.value != pswordConfirm.value){
         event.preventDefault();
         password.style.border = '2px solid red';
         pswordConfirm.style.border = '2px solid red';
-    }
+    } 
 
 })

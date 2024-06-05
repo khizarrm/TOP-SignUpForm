2a2667a9bbe6c197421933d2a5bff45f0ca2a845
Khizar Malik <khizarmalik2003@gmail.com>
2024-06-05 08:41:54 -0400

    Modified js. User name now shows up once they submit the form

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const submitButton = document.querySelector('button');
 submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
     const password = document.querySelector('input[type=password][name=password');
     const pswordConfirm = document.querySelector('input[type=password][name=password-confirm]');
 
     if (password.value != pswordConfirm.value){
-        event.preventDefault();
         password.style.border = '2px solid red';
         pswordConfirm.style.border = '2px solid red';
-    } 
+    } else {
+        const txtContainer = document.querySelector('.text-container > h1');
+        const formData = new FormData(document.querySelector('form'))
+        let fname = formData.get('fname');
+        let lname = formData.get('lname');
+        txtContainer.innerHTML += `<br><br>Welcome, ${fname} ${lname}!`;
+        console.log('all g');
+    }
 
 })
+
